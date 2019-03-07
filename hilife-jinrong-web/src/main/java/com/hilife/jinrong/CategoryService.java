894b1384a31f24c122cd1a4031e8e18d06f18595
fangzh <gaibiannana@163.com>
2019-03-07 16:29:55 +0800

    商品分类

---
+++ hilife-jinrong-web/src/main/java/com/hilife/jinrong/CategoryService.java
@@ -0,0 +1,15 @@
+package com.hilife.jinrong;
+
+/**
+ * Program Name: hilife-jinrong
+ * <p>
+ * Description:
+ * <p>
+ * Created by Administrator on 2019/3/7
+ *
+ * @author Administrator
+ * @version 1.0
+ */
+public class CategoryService {
+
+}
